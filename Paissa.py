b08e67a12d1f4fac38f8fec3972aaae7df2d6b34
nagaresst <nagaresst@live.com>
2022-04-28 15:03:53 +0800

    update

--- Paissa.py
+++ Paissa.py
@@ -2,3 +2,48 @@ import ttkbootstrap
 import tkinter
 from Queryer import Queryer
 
+
+class TopMenu(object):
+    def __int__(self):
+        pass
+
+
+class ModuleLoadPage(object):
+    def __int__(self):
+        pass
+
+
+class ModuleIndex(object):
+    def __int__(self):
+        pass
+
+
+class ModuleItemList(object):
+    def __int__(self):
+        pass
+
+
+class ModuleItemDetial(object):
+    def __int__(self):
+        pass
+
+
+class ModuleSaleList(object):
+    def __int__(self):
+        pass
+
+
+class ModuleSaleHistory(object):
+    def __int__(self):
+        pass
+
+
+class ModuleCraftCost(object):
+    def __int__(self):
+        pass
+
+
+class ModuleRelativeArticles(object):
+    def __int__(self):
+        pass
+

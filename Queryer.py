365ca6d0275ec73022235cdbe297e157de842189
Naga Resst <nagaresst@live.com>
2022-08-24 17:11:58 +0800

    更新注释

--- Queryer.py
+++ Queryer.py
@@ -119,11 +119,13 @@ class Queryer(object):
         根据当前查询的大区或者服务器，为查询所有区服最低价提供支持
         https://ff.web.sdo.com/web8/index.html#/servers
         跨大区开放后 server_list 将返回所有区服的列表
-        :return list:包含多个服务器的str的列表，去掉了大区名称
+        :return server_list -> list:包含多个服务器的str的列表，去掉了大区名称
         """
         select_server_mao = ['猫小胖', '紫水栈桥', '延夏', '静语庄园', '摩杜纳', '海猫茶屋', '柔风海湾', '琥珀原']
-        select_server_zhu = ['莫古力', '白银乡', '白金幻象', '神拳痕', '潮风亭', '旅人栈桥', '拂晓之间', '龙巢神殿', '梦羽宝境']
-        select_server_niao = ['陆行鸟', '红玉海', '神意之地', '拉诺西亚', '幻影群岛', '萌芽池', '宇宙和音', '沃仙曦染', '晨曦王座']
+        select_server_zhu = ['莫古力', '白银乡', '白金幻象', '神拳痕', '潮风亭', '旅人栈桥', '拂晓之间', '龙巢神殿',
+                             '梦羽宝境']
+        select_server_niao = ['陆行鸟', '红玉海', '神意之地', '拉诺西亚', '幻影群岛', '萌芽池', '宇宙和音', '沃仙曦染',
+                              '晨曦王座']
         select_server_gou = ['豆豆柴', '水晶塔', '银泪湖', '太阳海岸', '伊修加德', '红茶川']
         # 设置默认大区为猫区
         server_list = select_server_mao
@@ -141,8 +143,8 @@ class Queryer(object):
     def query_item_id(self, name):
         """
         查询官方的物品ID，为后面的查询提供支持
-        :param name:玩家输入的道具名称 可能仅为道具名的关键字
-        :return self.item_list
+        :param name:玩家输入的道具名称 可以仅为道具名的关键字
+        :return self.item_list -> list
         """
         self.item_list = []
         if self.static is False:
@@ -205,7 +207,7 @@ class Queryer(object):
         """
         大区内服务器比价，
         :param server_list: list 服务器列表
-        :return self.every_server
+        :return self.every_server -> list 根据服务器列表查询每个区的最低价
         """
         # 清空全服差价的结果列表
         self.every_server = []
@@ -239,6 +241,7 @@ class Queryer(object):
     def query_item_craft(self):
         """
         查询物品的制作材料
+        :return self.stuff -> dist 这个道具的制作配方
         """
         if len(self.stuff) == 0:
             if self.static is False:
@@ -263,7 +266,8 @@ class Queryer(object):
 
     def make_child_item_craft(self, unit):
         """
-        材料树递归查询的线程函数
+        材料树递归查询的线程函数 这个方法会直接把价格更新到实例的stuff属性里
+        :param unit -> dist 道具的数据
         """
         result = self.query_item_detial(unit['id'])
         unit['name'] = result['name']
@@ -286,6 +290,10 @@ class Queryer(object):
                 self.make_item_craft(unit['craft'])
 
     def make_child_item_craft_new(self, unit):
+        """
+        新的材料树递归查询的线程函数 这个方法会在一次接口请求中查询多个物品
+        :param unit -> dist 道具的数据
+        """
         result = self.query_item_detial(unit['id'])
         unit['name'] = result['name']
         items = []
@@ -314,6 +322,7 @@ class Queryer(object):
         """
         统计物品的制作材料
         新旧两种查询方法在大量查询的时候都会产生返回429的现象，导致查询速度慢，但新的查询方法在查询复杂配方的时候速度提升明显。
+        :param stuff_list -> list 包含多个unit的列表
         """
         threads = []
         if len(stuff_list) > 5:
@@ -336,6 +345,8 @@ class Queryer(object):
     def query_item_detial(self, itemid):
         """
         查询物品的详细信息，查询制作配方和统计成本的前置方法
+        :param itemid 需要初始化成字符串使用
+        :return result -> dist 物品的数据
         """
         if self.static is False:
             query_url = 'https://garlandtools.cn/api/get.php?type=item&lang=chs&version=3&id=' + str(itemid)
@@ -351,8 +362,10 @@ class Queryer(object):
 
     def query_item_cost_min(self, item):
         """
-        查询单项物品的板子价格
+        查询单项物品的板子价格，调用缓存或者是在线查询物品的最低价
+        :param item -> dist 物品的数据
         """
+        # 确定查询的服务器所在大区
         select_server_zhu = ['莫古力', '白银乡', '白金幻象', '神拳痕', '潮风亭', '旅人栈桥', '拂晓之间', '龙巢神殿',
                              '梦羽宝境']
         select_server_niao = ['陆行鸟', '红玉海', '神意之地', '拉诺西亚', '幻影群岛', '萌芽池', '宇宙和音', '沃仙曦染',
@@ -367,11 +380,13 @@ class Queryer(object):
         else:
             server = '猫小胖'
         if type(item) is not list:
+            # 缓存中没有数据，进行在线查询
             if item['id'] not in self.price_cache:
                 query_url = '/api/%s/%s?listings=1&noGst=true' % (server, item['id'])
                 result = self.init_query_result(query_url, 'universalis')
                 if len(result['listings']) == 0:
                     result['listings'].append({'pricePerUnit': 0})
+                # x参数  抗人为干扰
                 x = abs(result['averagePrice'] - result['listings'][0]['pricePerUnit'])
                 if int(item['id']) < 20:
                     # 碎晶，水晶，晶簇
@@ -380,16 +395,22 @@ class Queryer(object):
                     item['pricePerUnit'] = int(result['averagePrice'])
                 else:
                     item['pricePerUnit'] = result['listings'][0]['pricePerUnit']
+                # 更新缓存
                 self.price_cache[int(item['id'])] = copy.deepcopy(item['pricePerUnit'])
+            # 缓存命中，直接读取数据。
+            # 缓存没有超时时间，但是不会有人开一整天猴面雀吧
             else:
                 item['pricePerUnit'] = self.price_cache[item['id']]
+        # 一次查询多个物品 ，在计算成本的时候会用到
         elif type(item) is list:
             ids = []
+            # 先提取出没有缓存的物品ID,有缓存直接用缓存
             for i in item:
                 if i['id'] not in self.price_cache:
                     ids.append(str(i['id']))
                 else:
                     i['pricePerUnit'] = self.price_cache[i['id']]
+            # 把list转换成字符串，准备在线查询
             idss = ','.join(ids)
             if len(ids) > 1:
                 query_url = '/api/%s/%s?listings=1&noGst=true' % (server, idss)
@@ -398,6 +419,7 @@ class Queryer(object):
                 query_url = '/api/%s/%s?listings=1&noGst=true' % (server, idss)
                 result = [self.init_query_result(query_url, 'universalis')]
             for i in item:
+                # 把在线查询到的结果更新到缓存中
                 if i['id'] not in self.price_cache:
                     for r in result:
                         if str(r['itemID']) == str(i['id']):
@@ -416,9 +438,13 @@ class Queryer(object):
     def query_item_cost(self, stuff_list, count=1, tab=0):
         """
         查询物品的制作成本的计算器
+        :param stuff_list -> list 材料列表，包含多个unit
+        :param count -> int 制作次数
+        :param tab -> str 文本输出格式控制
         """
 
         def c_tab(c_str='', tab=0):
+            # 格式控制
             i = 0
             f_str = c_str
             m_str = '\t\t\t\t'
@@ -443,7 +469,7 @@ class Queryer(object):
             stuff['amount'] = n_count
             stuff['pricePerUnit'] = price
             d_cost += price
-
+            # 准备复制到剪贴板
             f_str, m_str = c_tab(tab=tab)
             self.clipboard = self.clipboard + '%s%s%s%d\t%d' % (f_str, stuff['name'], m_str, n_count, price) + '\n'
             # 如果这个东西可以被制作
@@ -467,6 +493,8 @@ class Queryer(object):
     def show_item_cost(self):
         """
         显示物品的制作成本的外壳
+        :return self.d_cost -> int 物品的直接材料制作成本
+        :return self.o_cost -> int 物品的原始材料成本
         """
         self.clipboard = '直接材料\t二级材料\t三级材料\t四级材料\t直接材料数量\t直接材料价值\t二级材料数量\t二级材料价值\t三级材料数量\t三级材料价值\t四级材料数量\t四级材料价值\n'
         start = time.time()
@@ -474,9 +502,12 @@ class Queryer(object):
         self.stuff = {}
         self.d_cost = 0
         self.o_cost = 0
+        # 查询配方
         self.query_item_craft()
+        # 确认到有配方
         if len(self.stuff) > 0:
             self.d_cost = self.query_item_cost(self.stuff['craft'])
+            # debug 计算查询用时
             end = time.time()
             print('材料树计算用时', end - start)
             self.clipboard = '%s\t\t直接材料成本\t%d\t\t原始材料成本\t%d\t\t更新时间\t%s\n\n' % (

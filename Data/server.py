7df147aa21987e468b93cf3eed2775e2e2acc840
Naga Resst <nagaresst@live.com>
2023-02-03 16:35:29 +0800

    更新查询国际服的服务器列表

---
+++ Data/server.py
@@ -0,0 +1,206 @@
+world =  [ { "id": 21, "name": "Ravana" },
+  { "id": 22, "name": "Bismarck" },
+  { "id": 23, "name": "Asura" },
+  { "id": 24, "name": "Belias" },
+  { "id": 28, "name": "Pandaemonium" },
+  { "id": 29, "name": "Shinryu" },
+  { "id": 30, "name": "Unicorn" },
+  { "id": 31, "name": "Yojimbo" },
+  { "id": 32, "name": "Zeromus" },
+  { "id": 33, "name": "Twintania" },
+  { "id": 34, "name": "Brynhildr" },
+  { "id": 35, "name": "Famfrit" },
+  { "id": 36, "name": "Lich" },
+  { "id": 37, "name": "Mateus" },
+  { "id": 39, "name": "Omega" },
+  { "id": 40, "name": "Jenova" },
+  { "id": 41, "name": "Zalera" },
+  { "id": 42, "name": "Zodiark" },
+  { "id": 43, "name": "Alexander" },
+  { "id": 44, "name": "Anima" },
+  { "id": 45, "name": "Carbuncle" },
+  { "id": 46, "name": "Fenrir" },
+  { "id": 47, "name": "Hades" },
+  { "id": 48, "name": "Ixion" },
+  { "id": 49, "name": "Kujata" },
+  { "id": 50, "name": "Typhon" },
+  { "id": 51, "name": "Ultima" },
+  { "id": 52, "name": "Valefor" },
+  { "id": 53, "name": "Exodus" },
+  { "id": 54, "name": "Faerie" },
+  { "id": 55, "name": "Lamia" },
+  { "id": 56, "name": "Phoenix" },
+  { "id": 57, "name": "Siren" },
+  { "id": 58, "name": "Garuda" },
+  { "id": 59, "name": "Ifrit" },
+  { "id": 60, "name": "Ramuh" },
+  { "id": 61, "name": "Titan" },
+  { "id": 62, "name": "Diabolos" },
+  { "id": 63, "name": "Gilgamesh" },
+  { "id": 64, "name": "Leviathan" },
+  { "id": 65, "name": "Midgardsormr" },
+  { "id": 66, "name": "Odin" },
+  { "id": 67, "name": "Shiva" },
+  { "id": 68, "name": "Atomos" },
+  { "id": 69, "name": "Bahamut" },
+  { "id": 70, "name": "Chocobo" },
+  { "id": 71, "name": "Moogle" },
+  { "id": 72, "name": "Tonberry" },
+  { "id": 73, "name": "Adamantoise" },
+  { "id": 74, "name": "Coeurl" },
+  { "id": 75, "name": "Malboro" },
+  { "id": 76, "name": "Tiamat" },
+  { "id": 77, "name": "Ultros" },
+  { "id": 78, "name": "Behemoth" },
+  { "id": 79, "name": "Cactuar" },
+  { "id": 80, "name": "Cerberus" },
+  { "id": 81, "name": "Goblin" },
+  { "id": 82, "name": "Mandragora" },
+  { "id": 83, "name": "Louisoix" },
+  { "id": 85, "name": "Spriggan" },
+  { "id": 86, "name": "Sephirot" },
+  { "id": 87, "name": "Sophia" },
+  { "id": 88, "name": "Zurvan" },
+  { "id": 90, "name": "Aegis" },
+  { "id": 91, "name": "Balmung" },
+  { "id": 92, "name": "Durandal" },
+  { "id": 93, "name": "Excalibur" },
+  { "id": 94, "name": "Gungnir" },
+  { "id": 95, "name": "Hyperion" },
+  { "id": 96, "name": "Masamune" },
+  { "id": 97, "name": "Ragnarok" },
+  { "id": 98, "name": "Ridill" },
+  { "id": 99, "name": "Sargatanas" },
+  { "id": 400, "name": "Sagittarius" },
+  { "id": 401, "name": "Phantom" },
+  { "id": 402, "name": "Alpha" },
+  { "id": 403, "name": "Raiden" },
+  { "id": 404, "name": "Marilith" },
+  { "id": 405, "name": "Seraph" },
+  { "id": 406, "name": "Halicarnassus" },
+  { "id": 407, "name": "Maduin" },
+  { "id": 1167, "name": "红玉海" },
+  { "id": 1081, "name": "神意之地" },
+  { "id": 1042, "name": "拉诺西亚" },
+  { "id": 1044, "name": "幻影群岛" },
+  { "id": 1060, "name": "萌芽池" },
+  { "id": 1173, "name": "宇宙和音" },
+  { "id": 1174, "name": "沃仙曦染" },
+  { "id": 1175, "name": "晨曦王座" },
+  { "id": 1172, "name": "白银乡" },
+  { "id": 1076, "name": "白金幻象" },
+  { "id": 1171, "name": "神拳痕" },
+  { "id": 1170, "name": "潮风亭" },
+  { "id": 1113, "name": "旅人栈桥" },
+  { "id": 1121, "name": "拂晓之间" },
+  { "id": 1166, "name": "龙巢神殿" },
+  { "id": 1176, "name": "梦羽宝境" },
+  { "id": 1043, "name": "紫水栈桥" },
+  { "id": 1169, "name": "延夏" },
+  { "id": 1106, "name": "静语庄园" },
+  { "id": 1045, "name": "摩杜纳" },
+  { "id": 1177, "name": "海猫茶屋" },
+  { "id": 1178, "name": "柔风海湾" },
+  { "id": 1179, "name": "琥珀原" },
+  { "id": 1192, "name": "水晶塔" },
+  { "id": 1183, "name": "银泪湖" },
+  { "id": 1180, "name": "太阳海岸" },
+  { "id": 1186, "name": "伊修加德" },
+  { "id": 1201, "name": "红茶川" },
+  { "id": 1068, "name": "黄金谷" },
+  { "id": 1064, "name": "月牙湾" },
+  { "id": 1187, "name": "雪松原" }
+]
+
+
+server = [
+  {
+    "name": "Elemental",
+    "region": "Japan",
+    "worlds": [ 45, 49, 50, 58, 68, 72, 90, 94 ]
+  },
+  {
+    "name": "Gaia",
+    "region": "Japan",
+    "worlds": [ 43, 46, 51, 59, 69, 76, 92, 98 ]
+  },
+  {
+    "name": "Mana",
+    "region": "Japan",
+    "worlds": [ 23, 28, 44, 47, 48, 61, 70, 96 ]
+  },
+  {
+    "name": "Aether",
+    "region": "North-America",
+    "worlds": [ 40, 54, 57, 63, 65, 73, 79, 99 ]
+  },
+  {
+    "name": "Primal",
+    "region": "North-America",
+    "worlds": [ 35, 53, 55, 64, 77, 78, 93, 95 ]
+  },
+  {
+    "name": "Chaos",
+    "region": "Europe",
+    "worlds": [ 39, 71, 80, 83, 85, 97, 400, 401 ]
+  },
+  {
+    "name": "Light",
+    "region": "Europe",
+    "worlds": [ 33, 36, 42, 56, 66, 67, 402, 403 ]
+  },
+  {
+    "name": "Crystal",
+    "region": "North-America",
+    "worlds": [ 34, 37, 41, 62, 74, 75, 81, 91 ]
+  },
+  { "name": "Materia", "region": "Oceania", "worlds": [ 21, 22, 86, 87, 88 ] },
+  {
+    "name": "Meteor",
+    "region": "Japan",
+    "worlds": [ 24, 29, 30, 31, 32, 52, 60, 82 ]
+  },
+  {
+    "name": "Dynamis",
+    "region": "North-America",
+    "worlds": [ 404, 405, 406, 407 ]
+  },
+  {
+    "name": "陆行鸟",
+    "region": "中国",
+    "worlds": [ 1167, 1081, 1042, 1044, 1060, 1173, 1174, 1175 ]
+  },
+  {
+    "name": "莫古力",
+    "region": "中国",
+    "worlds": [ 1172, 1076, 1171, 1170, 1113, 1121, 1166, 1176 ]
+  },
+  {
+    "name": "猫小胖",
+    "region": "中国",
+    "worlds": [ 1043, 1169, 1106, 1045, 1177, 1178, 1179 ]
+  },
+  {
+    "name": "豆豆柴",
+    "region": "中国",
+    "worlds": [ 1192, 1183, 1180, 1186, 1201, 1068, 1064, 1187 ]
+  }
+]
+
+list = []
+gs = []
+china = []
+for s in server:
+    if s['region'] == '中国':
+        china.append(s['name'])
+    else:
+        gs.append(s['name'])
+    ss = [s['name']]
+    for n in s['worlds']:
+        for w in world:
+            if w['id'] == n:
+                ss.append(w['name'])
+    list.append(ss)
+print(list)
+print("国际服大区")
+print("国服大区")

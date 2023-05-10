c3b123921f109befaa4b6d9c5fa556ad9b11108f
NagaResst <nagaresst@live.com>
2023-05-10 20:05:24 +0800

    修复版本差异性

--- Paissa.py
+++ Paissa.py
@@ -1,44 +1,53 @@
-from requests import get
-from Data.logger import logger
-import os
 import json
+import os
+
+from requests import get
+
+from Data.logger import logger
 
 """
 通过gitee拉取程序版本
 """
 try:
-    url = 'https://gitee.com/nagaresst/paissa/raw/master/Data/version'
+    url = 'https://raw.githubusercontent.com/NagaResst/Paissa/master/Data/version'
     version_online = json.loads(get(url, timeout=5).text)
-    logger.info("版本更新检查 Gitee Success, 主程序版本 {} ， 数据版本 {}".format(
+    logger.info("版本更新检查 Github Success, 主程序版本 {} ， 数据版本 {}".format(
         version_online['program'], version_online['data']))
 except:
     url = 'https://gitee.com/nagaresst/paissa/raw/master/Data/version'
     version_online = json.loads(get(url, timeout=5).text)
-    logger.info("版本更新检查 Github Success, 主程序版本 {} ， 数据版本 {}".format(
+    logger.info("版本更新检查 Gitee Success, 主程序版本 {} ， 数据版本 {}".format(
         version_online['program'], version_online['data']))
 
 """
 读取本地版本进行比对
 """
-history_file = os.path.join('Data', "Paissa_query_history.log")
-with open(history_file, 'r', encoding='utf-8') as his:
-    history_json = json.load(his)
-    program_version = history_json['program_version']
-    his.close()
-data_file = os.path.join('Data', "item.Pdt")
-with open(data_file, 'r', encoding='utf-8') as data:
-    data_json = json.load(data)
-    data_version = data_json['data-version']
-    data.close()
-logger.info("本地版本读取成功，主程序版本 {} ， 数据版本 {}".format(program_version, data_version))
+try:
+    history_file = os.path.join('Data', "Paissa_query_history.log")
+    with open(history_file, 'r', encoding='utf-8') as his:
+        history_json = json.load(his)
+        program_version = history_json['program_version']
+        his.close()
+except:
+    program_version = None
+
+try:
+    data_file = os.path.join('Data', "item.Pdt")
+    with open(data_file, 'r', encoding='utf-8') as data:
+        data_json = json.load(data)
+        data_version = data_json['data-version']
+        data.close()
+    logger.info("本地版本读取成功，主程序版本 {} ， 数据版本 {}".format(program_version, data_version))
+except:
+    data_version = None
 
 if version_online['program'] != program_version:
     try:
-        program_text = get('https://gitee.com/nagaresst/paissa/raw/master/Window.py', timeout=5).text
-        query_text = get('https://gitee.com/nagaresst/paissa/raw/master/Queryer.py', timeout=5).text
-    except:
         program_text = get('https://raw.githubusercontent.com/NagaResst/Paissa/master/Window.py', timeout=5).text
         query_text = get('https://raw.githubusercontent.com/NagaResst/Paissa/master/Queryer.py', timeout=5).text
+    except:
+        program_text = get('https://gitee.com/nagaresst/paissa/raw/master/Window.py', timeout=5).text
+        query_text = get('https://gitee.com/nagaresst/paissa/raw/master/Queryer.py', timeout=5).text
     with open('Window.py', 'w', encoding='utf-8') as program:
         program.write(program_text)
         program.close()
@@ -49,9 +58,9 @@ if version_online['program'] != program_version:
 
 if version_online['data'] != data_version:
     try:
-        data_text = get('https://gitee.com/nagaresst/paissa/raw/master/Data/item.Pdt', timeout=5).text
+        data_text = get('https://raw.githubusercontent.com/NagaResst/Paissa/master/Data/item.Pdt', timeout=5).text
     except:
-        data_text = get('https://github.com/NagaResst/Paissa/raw/master/Data/item.Pdt', timeout=5).text
+        data_text = get('https://gitee.com/nagaresst/paissa/raw/master/Data/item.Pdt', timeout=5).text
     with open(data_file, 'w', encoding='utf-8') as data:
         data.write(data_text)
         data.close()
